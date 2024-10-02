b3fd775e4e1a2abd1aaa678dfc8e7842930e7697
worksapce <289582304@qq.com>
2024-10-02 22:16:57 +0800

    test: 测试图标

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,6 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-      <link href="favicon.ico" rel="shortcuticon" type="image/x-icon"/>
         <Header />
         {children}
       </body>
